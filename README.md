9bdac26bd3031c3a35a4919a42e62baa9ffd0f90
Nathan Nichols <natenichols@cox.net>
2020-07-16 10:08:53 -0700

    practice script

---
+++ README.md
@@ -0,0 +1,5 @@
+Practice Script for printing ledger info every time a ledger is closed on the XRPL
+
+run:
+`yarn install`
+`node index.js`
---
+++ index.js
@@ -0,0 +1,33 @@
+const RippleAPI = require('ripple-lib').RippleAPI;
+
+const api = new RippleAPI({
+  server: 'wss://s1.ripple.com' // Public rippled server hosted by Ripple, Inc.
+});
+api.on('error', (errorCode, errorMessage) => {
+  console.log(errorCode + ': ' + errorMessage);
+});
+api.on('connected', () => {
+  console.log('connected');
+});
+api.on('disconnected', (code) => {
+    // code - [close code](https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent) sent by the server
+    // will be 1000 if this was normal closure
+    console.log('disconnected, code:', code);
+  });
+
+api.connect().then(() => {
+  // 'transaction' can be replaced with the relevant `type` from the table above
+  api.connection.on('ledgerClosed', (event) => {
+
+      // Do something useful with `event`
+      console.log(JSON.stringify(event, null, 2))
+  })
+
+  api.request('subscribe', {
+      streams: [ "ledgerClosed" ]
+  }).then(response => {
+      console.log('Successfully subscribed')
+  }).catch(error => {
+      // Handle `error`
+  })
+})
